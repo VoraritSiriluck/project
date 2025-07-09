f669c37894ce797d258f051157143a21ceea5c77
Vorarit Siriluck <chom2015chom@gmail.com>
2025-07-09 14:44:26 +0700

    Delete Data

---
+++ admin-delete.php
@@ -0,0 +1,23 @@
+<?php
+require_once('connection.php');
+
+
+if (isset($_REQUEST["delete_id"])) {
+    $id = $_REQUEST['delete_id'];
+    $select_stmt = $db->prepare("SELECT * FROM clean_report WHERE id=:id");
+    $select_stmt->bindParam(':id', $id);
+    $select_stmt->execute();
+    $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
+
+
+    $delete_stmt = $db->prepare('DELETE FROM clean_report WHERE id= :id');
+    $delete_stmt->bindParam(":id", $id);
+    $delete_stmt->execute();
+    //
+    if ($delete_stmt->execute()) {
+        $Delete = "";
+        header('Location: admin.php?deleteat_id=' . $Delete);
+        die();
+    }
+    //
+}
