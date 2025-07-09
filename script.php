57b24ad73fecdbb7db4d6cb60fc1c487cfd2deb8
Vorarit Siriluck <chom2015chom@gmail.com>
2025-07-09 14:41:49 +0700

    Head & Script

---
+++ script.php
@@ -0,0 +1,10 @@
+
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script> <!--jquery สำหรับ Datatable-->
+<script src="https://cdn.datatables.net/2.3.2/js/dataTables.js"></script> <!--javascript สำหรับ Datatable-->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+    crossorigin="anonymous"> //่javascript ของ bootstrap
+    
+</script>
