209d0648e7adc03e2a4fec59a5e3c38e8ff28673
Vorarit Siriluck <chom2015chom@gmail.com>
2025-07-24 08:38:55 +0700

    Login fix

--- login.php
+++ login.php
@@ -54,59 +54,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <body class="bg-primary bg-opacity-10">
 
-    <div class="row justify-content-center">
-
-        <div class="col-xl-10 col-lg-12 col-md-9">
-
-            <div class="card o-hidden border-0 shadow-lg " style="height:397px; margin-top:50px">
-                
-                    <!-- Nested Row within Card Body -->
-                    <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image">
-                            <img src="image/logo_osit.png" alt="" class="mx-auto d-block border-left-primary" style="width:80%; margin-top:110px;">
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <div class="row g-0 shadow rounded overflow-hidden">
+                    <!-- ซ้าย: รูปภาพ -->
+                    <div class="col-md-6 bg-white d-flex align-items-center justify-content-center p-4">
+                        <img src="image/logo_osit.png" class="img-fluid rounded" alt="ตัวอย่างภาพ">
+                    </div>
+                    <!-- ขวา: ฟอร์ม -->
+                    <div class="col-md-6 bg-primary text-white p-4">
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4 text-light">Cleaning Report Check System</h1>
                         </div>
-                        <div class="col-lg-6 bg-primary">
-                            <div class="p-5">
-                                <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4 text-light">Cleaning Report Check System</h1>
-                                </div>
-                                <hr>
-                                <?php if (isset($error)) : ?>
-                                    <div class="alert alert-danger mt-2">
-                                        <?php echo htmlspecialchars($error); ?>
-
-                                    </div>
-                                <?php endif; ?>
-                                <form class="user" method="POST">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control form-control-user mb-2" name="username" aria-describedby="emailHelp" placeholder="Username">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-user mb-2" name="password" placeholder="Password">
-                                    </div>
-                                    <div class="form-group">
-
-                                    </div>
-                                    <hr>
-                                    <button class="btn btn-success my-3 fs-5">Log-In>></button><br>
-                                    <div class="text-end mt-3">
-                                        <a class="icon-link icon-link-hover link-underline-secondary text-body-secondary" href="index.php" style="text-decoration: none;"><b>back</b>
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="bi" viewBox="0 0 16 16" aria-hidden="true">
-                                                <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                                            </svg>
-                                        </a>
-                                    </div>
-
-                                    <!-- <input type="submit" class="btn btn-success my-3 fs-5 " value="Log-In>>"> -->
-                                </form>
+                        <?php if (isset($error)) : ?>
+                            <div class="alert alert-danger mt-2">
+                                <?php echo htmlspecialchars($error); ?>
                             </div>
+                        <?php endif; ?>
+                        <hr>
+                        <form method="POST" class="user">
+                            <!-- User -->
+                            <div class="form-group mb-3">
+                                <input type="text" class="form-control form-control-user mb-2" name="username" aria-describedby="emailHelp" placeholder="Username">
+                            </div>
+                            <!-- Password -->
+                            <div class="form-group mb-3">
+                                <input type="password" class="form-control form-control-user mb-2" name="password" placeholder="Password">
+                            </div>
+                            <hr>
+                            <button class="btn btn-success my-3 fs-5 ">Log-In>></button><br>
+                        </form>
+                        <div class="text-end mt-3">
+                            <a class="icon-link icon-link-hover link-underline-secondary text-body-secondary" href="index.php" style="text-decoration: none;"><b>back</b>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="bi" viewBox="0 0 16 16" aria-hidden="true">
+                                    <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                </svg>
+                            </a>
                         </div>
                     </div>
-                
+                </div>
             </div>
-
         </div>
-
     </div>
 
 
